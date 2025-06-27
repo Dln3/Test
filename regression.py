5faf1891cb2a3f1e27dd128425760e75ecdddb2a
Dln3 <121076981+Dln3@users.noreply.github.com>
2025-06-27 16:31:32 -0700

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,60 +1,67 @@
+#!/usr/bin/env python3
+
 import angr
 import claripy
 
 MAX_ARG_LEN = 8
 NUM_ARGS = 5
 
-def symbolic_args():
+def make_symbolic_args():
     args = []
     for i in range(NUM_ARGS):
-        arg = claripy.BVS(f'arg{i}', MAX_ARG_LEN * 8)  # 8 bits per char
-        args.append(arg)
+        chars = [claripy.BVS(f'arg{i}_{j}', 8) for j in range(MAX_ARG_LEN)]
+        arg = claripy.Concat(*chars + [claripy.BVV(0, 8)])  # Null-terminated
+        args.append((arg, chars))  # Store both for later decoding
     return args
 
-def create_state(proj, sym_args):
-    # Construct argv with filename as first arg, then symbolic args
-    argv = [proj.filename]
-    for arg in sym_args:
-        # Add null terminator for each string and limit length to MAX_ARG_LEN
-        argv.append(arg)
-    state = proj.factory.full_init_state(args=argv)
+def create_state(project, symbolic_args):
+    args = [project.filename] + [arg for arg, _ in symbolic_args]
+    state = project.factory.full_init_state(args=args)
 
-    # Constrain symbolic arguments to be printable and null-terminated within 8 bytes
-    for arg in sym_args:
-        for i in range(MAX_ARG_LEN):
-            c = arg.get_byte(i)
+    # Fill unknown memory safely
+    state.options.add(angr.options.ZERO_FILL_UNCONSTRAINED_MEMORY)
+
+    # Constrain args to printable ASCII
+    for _, chars in symbolic_args:
+        for c in chars:
             state.solver.add(c >= 0x20)
             state.solver.add(c <= 0x7e)
+
     return state
 
-def get_terminated_states(proj, sym_args):
-    state = create_state(proj, sym_args)
-    simgr = proj.factory.simgr(state)
+def run_and_get_stdout(project, symbolic_args):
+    state = create_state(project, symbolic_args)
+    simgr = project.factory.simulation_manager(state)
     simgr.run()
     return simgr.deadended
 
 def main():
-    proj1 = angr.Project('bin/version1', auto_load_libs=False)
-    proj2 = angr.Project('bin/version2', auto_load_libs=False)
+    proj1 = angr.Project("bin/version1", auto_load_libs=False)
+    proj2 = angr.Project("bin/version2", auto_load_libs=False)
 
-    sym_args = symbolic_args()
+    symbolic_args = make_symbolic_args()
 
-    states_v1 = get_terminated_states(proj1, sym_args)
-    states_v2 = get_terminated_states(proj2, sym_args)
+    states_v1 = run_and_get_stdout(proj1, symbolic_args)
+    states_v2 = run_and_get_stdout(proj2, symbolic_args)
 
     for s1 in states_v1:
         for s2 in states_v2:
-            # Check for diverging behavior - here: different exit codes
-            if s1.solver.satisfiable(extra_constraints=[s1.regs.eax != s2.regs.eax]):
-                m = s1.solver.min
+            # Compare stdout for divergence
+            out1 = s1.posix.dumps(1)
+            out2 = s2.posix.dumps(1)
+            if s1.solver.satisfiable(extra_constraints=[out1 != out2]):
+                # Concrete solution
+                solver = s1.solver
                 with open("regressing.txt", "w") as f:
-                    for arg in sym_args:
-                        val = m(s1.solver.eval(arg, cast_to=bytes).rstrip(b'\x00'))
-                        f.write(val.decode('utf-8', errors='ignore') + " ")
+                    for arg, chars in symbolic_args:
+                        bytestr = b''.join([solver.eval(c, cast_to=bytes) for c in chars])
+                        printable = bytestr.rstrip(b'\x00').decode('utf-8', errors='ignore')
+                        f.write(printable + " ")
                     f.write("\n")
-                print("Regression found! Inputs written to regressing.txt")
+                print("[+] Regression found! Written to regressing.txt")
                 return
-    print("No regression found.")
 
-if __name__ == '__main__':
+    print("[-] No regression detected.")
+
+if __name__ == "__main__":
     main()
