bb8c607f11f2632fbce3ba83b42ec1978fba1a0d
Dln3 <121076981+Dln3@users.noreply.github.com>
2025-06-27 16:58:42 -0700

    Update regression.py

--- regression.py
+++ regression.py
@@ -3,63 +3,66 @@
 import angr
 import claripy
 
-def main():
-    binary1 = 'bin/version1'
-    binary2 = 'bin/version2'
-    num_args = 5
-    max_arg_len = 8
+MAX_ARG_LEN = 8
+NUM_ARGS = 5
+MAX_STEPS = 500  # Limit symbolic execution steps to avoid crashes
 
-    # Create symbolic arguments (with null terminator)
-    symbolic_args = []
-    for i in range(num_args):
-        chars = [claripy.BVS(f'arg{i}_{j}', 8) for j in range(max_arg_len)]
-        arg = claripy.Concat(*chars + [claripy.BVV(0, 8)])
-        symbolic_args.append((arg, chars))  # Keep chars for decoding
+def make_symbolic_args():
+    args = []
+    for i in range(NUM_ARGS):
+        chars = [claripy.BVS(f'arg{i}_{j}', 8) for j in range(MAX_ARG_LEN)]
+        arg = claripy.Concat(*chars + [claripy.BVV(0, 8)])  # Null-terminated
+        args.append((arg, chars))
+    return args
 
-    argv1 = [binary1] + [arg for arg, _ in symbolic_args]
-    argv2 = [binary2] + [arg for arg, _ in symbolic_args]
+def create_state(project, symbolic_args):
+    args = [project.filename] + [arg for arg, _ in symbolic_args]
+    state = project.factory.full_init_state(args=args)
 
-    proj1 = angr.Project(binary1, auto_load_libs=False)
-    proj2 = angr.Project(binary2, auto_load_libs=False)
+    # Zero-fill unconstrained memory & registers to prevent strlen issues & warnings
+    state.options.add(angr.options.ZERO_FILL_UNCONSTRAINED_MEMORY)
+    state.options.add(angr.options.ZERO_FILL_UNCONSTRAINED_REGISTERS)
 
-    state1 = proj1.factory.full_init_state(args=argv1)
-    state2 = proj2.factory.full_init_state(args=argv2)
-
-    # Zero-fill unknown memory to avoid strlen issues
-    state1.options.add(angr.options.ZERO_FILL_UNCONSTRAINED_MEMORY)
-    state2.options.add(angr.options.ZERO_FILL_UNCONSTRAINED_MEMORY)
-
-    # Constrain input bytes to printable ASCII
+    # Constrain symbolic bytes to printable ASCII range
     for _, chars in symbolic_args:
         for c in chars:
-            state1.solver.add(c >= 0x20, c <= 0x7e)
-            state2.solver.add(c >= 0x20, c <= 0x7e)
+            state.solver.add(c >= 0x20)
+            state.solver.add(c <= 0x7e)
 
-    sm1 = proj1.factory.simulation_manager(state1)
-    sm2 = proj2.factory.simulation_manager(state2)
+    return state
 
-    sm1.run()
-    sm2.run()
+def run_symbolic(project, symbolic_args):
+    state = create_state(project, symbolic_args)
+    simgr = project.factory.simgr(state)
+    simgr.run(n=MAX_STEPS)  # Limit steps to avoid OOM/crashes
+    return simgr.deadended
 
-    for s1 in sm1.deadended:
-        for s2 in sm2.deadended:
+def main():
+    proj1 = angr.Project("bin/version1", auto_load_libs=False)
+    proj2 = angr.Project("bin/version2", auto_load_libs=False)
+
+    symbolic_args = make_symbolic_args()
+
+    states_v1 = run_symbolic(proj1, symbolic_args)
+    states_v2 = run_symbolic(proj2, symbolic_args)
+
+    for s1 in states_v1:
+        for s2 in states_v2:
             out1 = s1.posix.dumps(1)
             out2 = s2.posix.dumps(1)
 
             if s1.solver.satisfiable(extra_constraints=[out1 != out2]):
-                # Regression found!
-                concrete_args = []
-                for _, chars in symbolic_args:
-                    concrete = b''.join([s1.solver.eval(c, cast_to=bytes) for c in chars])
-                    concrete_args.append(concrete.rstrip(b'\x00'))
-
+                # Found regression
+                solver = s1.solver
                 with open("regressing.txt", "wb") as f:
-                    f.write(b" ".join(concrete_args))
+                    for _, chars in symbolic_args:
+                        concrete = b''.join([solver.eval(c, cast_to=bytes) for c in chars])
+                        f.write(concrete.rstrip(b'\x00') + b" ")
+                    f.write(b"\n")
 
-                print("[+] Regression found!")
-                print(f"version1 stdout: {out1}")
-                print(f"version2 stdout: {out2}")
-                print("[+] Written to regressing.txt")
+                print("[+] Regression found! Input saved in regressing.txt")
+                print(f"version1 output: {out1}")
+                print(f"version2 output: {out2}")
                 return
 
     print("[-] No regression found.")
